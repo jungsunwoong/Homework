9c0dcd03f86b8aab5544cd1090e61aee8fbd0802
Jungsunwoong <wjdtjsdnd12@naver.com>
2025-04-30 15:10:02 +0900

    Add : 10 컬렉션 기본 완료

---
+++ 03 백엔드 개발을 위한 자바 프로그래밍/기본/10 컬렉션/exam10/src/ch15/sec06/exam01/StackExample.java
@@ -0,0 +1,17 @@
+package ch15.sec06.exam01;
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(100);
+        stack.push(50);
+        stack.push(500);
+        stack.push(10);
+//        System.out.println(stack.size());
+        for (int i = 0 ; i < 4; i++) {
+            System.out.println("꺼내온 동전 : " + stack.pop());
+        }
+    }
+}
