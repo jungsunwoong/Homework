b02bfea0566d83bd50322184cbb76fbc082b2363
Jungsunwoong <wjdtjsdnd12@naver.com>
2025-05-08 17:48:34 +0900

    Add : 12 스트림 기본 풀이 중

---
+++ 03 백엔드 개발을 위한 자바 프로그래밍/01.기본/12 스트림/exam12/src/ch17/sec03/StreamPipeLineExample.java
@@ -0,0 +1,28 @@
+package ch17.sec03;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamPipeLineExample {
+    public static void main(String[] args) {
+        List<Student> list = Arrays.asList(
+                new Student("홍길동", 10),
+                new Student("신용권", 20),
+                new Student("유미선", 30)
+        );
+//        Stream<Student> stream = list.stream();
+//
+//        IntStream scoreStream = stream.mapToInt ( student -> student.getScore());
+//        double avg = scoreStream.average().getAsDouble();
+
+        double avg = list.stream()
+                        .mapToInt(student -> student.getScore())
+                                .average()
+                                        .getAsDouble();
+
+        System.out.println("평균 점수 : " + avg);
+    }
+
+}
