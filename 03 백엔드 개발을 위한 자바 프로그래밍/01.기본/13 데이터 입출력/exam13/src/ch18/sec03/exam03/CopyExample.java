5c904094012d5403c1a6bb174bfac8123d556a20
Jungsunwoong <wjdtjsdnd12@naver.com>
2025-05-13 19:57:09 +0900

    Add : 13 데이터 입출력 기본 풀이 중 & 기본&심화 정답 추가 / Mysql 02 데이터 베이스 모델링 기본 완료

---
+++ 03 백엔드 개발을 위한 자바 프로그래밍/01.기본/13 데이터 입출력/exam13/src/ch18/sec03/exam03/CopyExample.java
@@ -0,0 +1,7 @@
+package ch18.sec03.exam03;
+public class CopyExample {
+    public static void main(String[] args) throws Exception {
+        String originalFileName = "C:/Temp/test.jpg";
+        String targetFileName = "C:/Temp/test2.jpg";
+    }
+}
