cc523f4e274c387f8ef7b00d5e7a190e3d308b15
Jungsunwoong <wjdtjsdnd12@naver.com>
2025-05-28 10:43:53 +0900

    add : 08 java 연동 jdbc 프로그래밍

---
+++ 04 MySQL 기반 RDBMS 활용과 JDBC/01 기본/08 08 Java 연동 JDBC 프로그래밍-Travel(기본)/travelapp/src/main/java/org/scoula/database/JDBCUtil.java
@@ -0,0 +1,16 @@
+package org.scoula.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class JDBCUtil {
+    static Connection con = null;
+
+    public static Connection getConnection() {
+        return null;
+    }
+
+    public static void close() {
+    }
+}
